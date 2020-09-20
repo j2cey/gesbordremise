2bd0ab8b4b323616740a9804b03d9de4ab611f74
j2cey <jud10parfait@gmail.com>
2020-09-20 04:40:11 +0000

    set custom Base Migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Laravel Macros are great way of extending Laravel's core classes and add extra functionality
+         * required for our application.
+         * It is a way to add somme missing functionality to Laravel's internal component with a piece of code
+         * that doesn't exist in the Laravel class.
+         *
+         * Blueprint is macroable, so we can just define a macro on it in this service provider to get base fields
+         */
+        Blueprint::macro('baseFields', function () {
+            $this->uuid('uuid');
+            $this->string('tags')->nullable()->comment('Tags, if any');
+            $this->foreignId('status_id')->nullable()
+                ->comment('status reference')
+                ->constrained()->onDelete('set null');
+            $this->boolean('is_default')->default(false)->comment('determine whether is the default one.');
+            $this->timestamps();
+        });
+        Blueprint::macro('dropBaseForeigns', function () {
+            $this->dropForeign(['status_id']);
+        });
     }
 }
