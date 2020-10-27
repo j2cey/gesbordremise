3c43087b09b8f60226f329791be4d671ded8d0f0
j2cey <jud10parfait@gmail.com>
2020-10-27 23:01:41 +0000

    Adldap2 issue

--- config/ldap.php
+++ config/ldap.php
@@ -80,160 +80,18 @@ return [
 
             'settings' => [
 
-                /*
-                |--------------------------------------------------------------------------
-                | Schema
-                |--------------------------------------------------------------------------
-                |
-                | The schema class to use for retrieving attributes and generating models.
-                |
-                | You can also set this option to `null` to use the default schema class.
-                |
-                | For OpenLDAP, you must use the schema:
-                |
-                |   Adldap\Schemas\OpenLDAP::class
-                |
-                | For FreeIPA, you must use the schema:
-                |
-                |   Adldap\Schemas\FreeIPA::class
-                |
-                | Custom schema classes must implement Adldap\Schemas\SchemaInterface
-                |
-                */
-
                 'schema' => Adldap\Schemas\ActiveDirectory::class,
-
-                /*
-                |--------------------------------------------------------------------------
-                | Account Prefix
-                |--------------------------------------------------------------------------
-                |
-                | The account prefix option is the prefix of your user accounts in LDAP directory.
-                |
-                | This string is prepended to all authenticating users usernames.
-                |
-                */
-
                 'account_prefix' => env('LDAP_ACCOUNT_PREFIX', ''),
-
-                /*
-                |--------------------------------------------------------------------------
-                | Account Suffix
-                |--------------------------------------------------------------------------
-                |
-                | The account suffix option is the suffix of your user accounts in your LDAP directory.
-                |
-                | This string is appended to all authenticating users usernames.
-                |
-                */
-
                 'account_suffix' => env('LDAP_ACCOUNT_SUFFIX', ''),
-
-                /*
-                |--------------------------------------------------------------------------
-                | Domain Controllers
-                |--------------------------------------------------------------------------
-                |
-                | The domain controllers option is an array of servers located on your
-                | network that serve Active Directory. You can insert as many servers or
-                | as little as you'd like depending on your forest (with the
-                | minimum of one of course).
-                |
-                | These can be IP addresses of your server(s), or the host name.
-                |
-                */
-
-                'hosts' => explode(' ', env('LDAP_HOSTS', '192.168.5.228')),
-
-                /*
-                |--------------------------------------------------------------------------
-                | Port
-                |--------------------------------------------------------------------------
-                |
-                | The port option is used for authenticating and binding to your LDAP server.
-                |
-                */
-
+                'hosts' => explode(' ', env('LDAP_HOSTS', 'corp-dc1.corp.acme.org corp-dc2.corp.acme.org')),
                 'port' => env('LDAP_PORT', 389),
-
-                /*
-                |--------------------------------------------------------------------------
-                | Timeout
-                |--------------------------------------------------------------------------
-                |
-                | The timeout option allows you to configure the amount of time in
-                | seconds that your application waits until a response
-                | is received from your LDAP server.
-                |
-                */
-
                 'timeout' => env('LDAP_TIMEOUT', 5),
-
-                /*
-                |--------------------------------------------------------------------------
-                | Base Distinguished Name
-                |--------------------------------------------------------------------------
-                |
-                | The base distinguished name is the base distinguished name you'd
-                | like to perform query operations on. An example base DN would be:
-                |
-                |        dc=corp,dc=acme,dc=org
-                |
-                | A correct base DN is required for any query results to be returned.
-                |
-                */
-
-                'base_dn' => env('LDAP_BASE_DN', 'dc=corp,dc=acme,dc=org'),
-
-                /*
-                |--------------------------------------------------------------------------
-                | LDAP Username & Password
-                |--------------------------------------------------------------------------
-                |
-                | When connecting to your LDAP server, a username and password is required
-                | to be able to query and run operations on your server(s). You can
-                | use any user account that has these permissions. This account
-                | does not need to be a domain administrator unless you
-                | require changing and resetting user passwords.
-                |
-                */
-
-                'username' => env('LDAP_USERNAME', 'gestockdsi@gabontelecom.ga'),
-                'password' => env('LDAP_PASSWORD', 'Libertis123'),
-
-                /*
-                |--------------------------------------------------------------------------
-                | Follow Referrals
-                |--------------------------------------------------------------------------
-                |
-                | The follow referrals option is a boolean to tell active directory
-                | to follow a referral to another server on your network if the
-                | server queried knows the information your asking for exists,
-                | but does not yet contain a copy of it locally.
-                |
-                | This option is defaulted to false.
-                |
-                */
-
-                'follow_referrals' => false,
-
-                /*
-                |--------------------------------------------------------------------------
-                | SSL & TLS
-                |--------------------------------------------------------------------------
-                |
-                | If you need to be able to change user passwords on your server, then an
-                | SSL or TLS connection is required. All other operations are allowed
-                | on unsecured protocols.
-                |
-                | One of these options are definitely recommended if you
-                | have the ability to connect to your server securely.
-                |
-                */
-
+                'base_dn' => env('LDAP_BASE_DN', 'ou=corp,dc=acme,dc=org'),
+                'username' => env('LDAP_USERNAME', ''),
+                'password' => env('LDAP_PASSWORD', ''),
+                'follow_referrals' => env('LDAP_FOLLOW_REFERRALS', false),
                 'use_ssl' => env('LDAP_USE_SSL', false),
                 'use_tls' => env('LDAP_USE_TLS', false),
-
             ],
 
         ],
